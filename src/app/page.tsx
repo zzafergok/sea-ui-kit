029613a0874d10377030c962c555d0d22693e570
zafergok <gok.zaferr@gmail.com>
2025-05-20 20:41:08 +0300

    fixed files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
+
+import React from 'react'
+
 import { type LoginFormValues } from '@/lib/validations/auth'
 
 // SSR devre dışı bırakılmış dinamik import
@@ -10,19 +12,8 @@ const LoginForm = dynamic(() => import('@/components/auth/LoginForm').then((mod)
 })
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    // İstemci tarafında olduğumuzu doğrula
-    setIsLoading(false)
-  }, [])
-
   const handleSubmit = (data: LoginFormValues) => {
     console.log(data)
-  }
-
-  if (isLoading) {
-    return <div className='min-h-screen flex items-center justify-center'>Yükleniyor...</div>
   }
 
   return <LoginForm onSubmit={handleSubmit} />
