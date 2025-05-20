646d95be10585f609336d7448627e65aa557eece
zafergok <gok.zaferr@gmail.com>
2025-05-20 20:38:14 +0300

    fixed files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from 'next'
-import { Bai_Jamjuree } from 'next/font/google'
 
 import React from 'react'
 
 import { Providers } from '@/providers/Providers'
 
 import '../styles/global.scss'
-
-const baiJamjuree = Bai_Jamjuree({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-bai-jamjuree',
-})
 
 export const metadata: Metadata = {
   title: 'Sea UI Kit Demo',
@@ -21,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='tr'>
-      <body className={`${baiJamjuree.variable} antialiased`}>
+      <body className='antialiased'>
         <Providers>{children}</Providers>
       </body>
     </html>
