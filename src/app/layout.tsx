b0177ee6f6abc686fa3f1578f26767ab8bc7a12d
zafergok <gok.zaferr@gmail.com>
2025-05-20 20:34:40 +0300

    fixed files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 import { Providers } from '@/providers/Providers'
 
-import '../styles/globals.scss'
+import '../styles/global.scss'
 
 const baiJamjuree = Bai_Jamjuree({
   subsets: ['latin'],
